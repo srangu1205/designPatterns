eb0e45cb3b5055daae81919c6f68c27d54f3c9a3
Sunil Rangu <sunil.rangu80@gmail.com>
2021-01-21 21:37:45 -0500

    intial project setup

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using BusinessModels;
+using IContracts;
+using System;
+using Factory;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        private static IRoleBase admin = FactoryPool.Create("admin");
+        static void Main(string[] args)
+        {
+            setAdmin();
+
+        }
+        private static void setAdmin()
+        {
+            admin.FirstName = "firstName";
+            admin.LastName = "lastName";
+            admin.Email = "abc@gmail.com";
+            admin.UserID = "abcd";
+            admin.Address = "address";
+            admin.SSN = "087-868-7998";
+          
+        }
+    }
+}
